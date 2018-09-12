151f879f75578eb426d2755bab7bd372cd87a4fc
Dylan Stratton <dm.stratton94@gmail.com>
2018-09-12 15:19:45 -0500

    slow the noise down

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function draw() {
     for (let x = width / 3; x < 2 * width / 3; x += spacing) {
       fill(
         noise(y / 250, frameCount / 500, x / 100) * 255,
-        noise(x / 3.14, frameCount / 250, y / 150) * 255,
-        noise(frameCount / 300, frameCount / 200, y / 200) * 255
+        noise(x / 34, frameCount / 250, y / 150) * 255,
+        noise(frameCount / 300, x / 200, y / 200) * 255
       );
       rect(x, y, spacing * 2, spacing * 2);
     }
